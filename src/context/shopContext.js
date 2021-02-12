e6f3756a3d982982e28d3f3829130bbd094f58a5
alexjbustillos <alexjbustillos@gmail.com>
2021-02-11 23:22:17 -0500

    [product on home page]

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -1,16 +1,73 @@
 import React, { Component } from 'react'
 import Client from 'shopify-buy'
 
-const client = React.build
+const ShopContext = React.createContext()
+
+const client = Client.buildClient({
+    storefrontAccessToken: process.env.REACT_APP_ACCESS_TOKEN,
+    domain: process.env.REACT_APP_DOMAIN
+})
 
 class ShopProvider extends Component {
+    
+    state = {
+        products: [],
+        product: {},
+        checkout: {},
+        isCartOpen: false,
+        test: 'test'
+    }
+
+    componentDidMount() {
+        this.createCheckout()
+    }
+
+    createCheckout = async () => {
+        const checkout = await client.checkout.create()
+        this.setState({ checkout: checkout })
+    }
+
+    addItemToCheckout = async (variantId, quantity) => {
+        const lineItemsToAdd = [{
+            variantId,
+            quantity: parseInt(quantity, 10)
+        }]
+        const checkout = await client.checkout.addLineItems(this.state.checkout.id, lineItemsToAdd)
+        this.setState({ checkout: checkout })
+    }
+    
+    fetchAllProducts = async () => {
+        const products = await client.product.fetchAll()
+        this.setState({ products: products })
+    }
+    
+    fetchProductWithId = async (id) => {
+        const product = await client.product.fetch(id)
+        this.setState({ product: product })
+    }
+    
+    closeCart = () => { this.setState({ isCartOpen: false }) }
+    
+    openCart = () => { this.setState({ isCartOpen: true }) }
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <ShopContext.Provider value={{
+                ...this.state,
+                fetchAllProducts: this.fetchAllProducts,
+                fetchProductWithId: this.fetchProductWithId,
+                closeCart: this.closeCart,
+                openCart: this.openCart,
+                addItemToCheckout: this.addItemToCheckout 
+            }} >
+                {this.props.children}
+            </ShopContext.Provider>
         )
     }
 }
+
+const ShopConsumer = ShopContext.Consumer
+
+export { ShopConsumer, ShopContext }
 
 export default ShopProvider
